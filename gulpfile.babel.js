58f6e8badbd85e7235371a7f8a9fc143027e8a1e
yejin <yejin@ibleaders.co.kr>
2021-02-14 17:37:51 +0900

    #1.0 - Gulp Task 이해
    - gulp-pug를 이용하여  .pug 파일을 html파일로 변환해 주는 예제 구현
    - .src(), .pipe(), .series() 이해

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,3 +3,18 @@
     Description: src의 *.pug을 이용하여 src 내부 파일들을 build 폴더에 html로 변환 해주는 예제  
 */
 import gulp from "gulp";
+// https://www.npmjs.com/package/gulp-pug
+import gpug from "gulp-pug";
+// 기존 Task와 충돌 할 수 있으므로 build 폴더를 clear 한 뒤에 Task 실행
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest:"build"
+    },
+}
+const pug = () => 
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest)); 
+export const dev = gulp.series([pug]);
