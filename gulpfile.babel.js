052e21b06aa98c6a2581c9ad6a1c6b069ce4b288
yejin <yejin@ibleaders.co.kr>
2021-02-14 17:45:32 +0900

    #2.0 - 웹 서버 만들기
    - gulp-webserver 모듈을 사용하여 웹 서버 구축

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,8 @@ import gulp from "gulp";
 // https://www.npmjs.com/package/gulp-pug
 import gpug from "gulp-pug";
 import del from "del";
+// https://www.npmjs.com/package/gulp-webserver
+import ws from "gulp-webserver";
 // 기존 Task와 충돌 할 수 있으므로 build 폴더를 clear 한 뒤에 Task 실행
 const routes = {
     pug: {
@@ -24,4 +26,9 @@ const clean = () => del(["build"]);
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
-export const dev = gulp.series([prepare, assets]);
+
+const webserver = () => gulp.src("build").pipe(ws({port: 8088, livereload: true, open: true}))
+const postDev = gulp.parallel([webserver, watch]);
+
+// export는 package.json에서 사용할 때 쓰면 댐
+export const dev = gulp.series([prepare, assets, postDev]);
